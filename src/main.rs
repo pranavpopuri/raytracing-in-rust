205ab24a586de46262e419e934324e3dc256f34a
Junhao Zhang <jerry_mountain@hotmail.com>
2025-10-26 11:20:02 -0500

    Create Branch
    Co-authored-by: Pranav Popuri <hipranav7@gmail.com>
    Co-authored-by: Shanrong Wu <ShanrongW@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,7 @@ fn main() {
         }
     }
 
-    // img.save("src/plus.ppm").unwrap();
     save("src/plus.ppm", &img).unwrap();
-
-    // let mut img = RgbImage::new(32, 32);
-
-    // for x in 15..=17 {
-    //     for y in 8..24 {
-    //         img.put_pixel(x, y, Rgb([255, 0, 0]));
-    //         img.put_pixel(y, x, Rgb([255, 0, 0]));
-    //     }
-    // }
-
-    // img.save("src/plus.png").unwrap();
 }
 
 /*
