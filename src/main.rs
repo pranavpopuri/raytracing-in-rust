f5237ed5283b75689d75e591fe446577bdd7d4db
Shanrong <shanrong06@gmail.com>
2025-10-28 11:02:48 -0500

    got to sphere part of tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,68 @@
-mod ppm_save;
-
-use image::{Rgb, RgbImage};
-
-use crate::ppm_save::save;
-
-fn main() {
-    println!("Hello, world!");
-
-    let mut img: image::ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(32, 32);
-
-    for x in 15..=17 {
-        for y in 8..24 {
-            img.put_pixel(x, y, Rgb([255, 0, 0]));
-            img.put_pixel(y, x, Rgb([255, 0, 0]));
-        }
-    }
-
-    // img.save("src/plus.ppm").unwrap();
-    save("src/plus.ppm", &img).unwrap();
-
-    // let mut img = RgbImage::new(32, 32);
-
-    // for x in 15..=17 {
-    //     for y in 8..24 {
-    //         img.put_pixel(x, y, Rgb([255, 0, 0]));
-    //         img.put_pixel(y, x, Rgb([255, 0, 0]));
-    //     }
-    // }
-
-    // img.save("src/plus.png").unwrap();
+mod color;
+mod ray;
+mod vec3;
+ 
+use std::io;
+ 
+use color::Color;
+use ray::Ray;
+use vec3::{Point3, Vec3};
+ 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * vec3::dot(oc, r.direction());
+    let c = vec3::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
 }
-
-/*
+ 
+fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+ 
+    let unit_direction = vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+ 
 fn main() {
     // Image
-
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
-
+ 
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+ 
+    // Camera
+ 
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+ 
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+ 
     // Render
-
+ 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-
+ 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            print!("{} {} {}\n", ir, ig, ib);
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = ray_color(&r);
+            color::write_color(&mut io::stdout(), pixel_color);
         }
     }
+ 
+    eprint!("\nDone.\n");
 }
-*/
