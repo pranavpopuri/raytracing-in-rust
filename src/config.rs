5ad180cddd5a63b7a346ac4feda0d250a93bb72b
Pranav Popuri <hipranav7@gmail.com>
2025-11-08 18:22:28 -0600

    changed SAMPLES_PER_PIXEL back to 100

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 pub const ASPECT_RATIO: f64 = 3.0 / 2.0;
 pub const IMAGE_WIDTH: i32 = 512;
 pub const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-pub const SAMPLES_PER_PIXEL: i32 = 1000;
+pub const SAMPLES_PER_PIXEL: i32 = 100;
 pub const MAX_DEPTH: i32 = 50;
